0a37477473e5caae74d0a18410bab7c4ea8e4c18
Cecilia urrutia <urrutia.cici@gmail.com>
2023-08-20 21:55:03 -0300

    Calculo de intervalo entre dos fechas para ciudadania

---
+++ dif_dos_fechas.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from dateutil import relativedelta
+
+from datetime import datetime
+
+fecha_1 = input("Ingrese la primera fecha en dd/mm/aaaa: ")
+fecha_2 = input("Ingrese la segunda fecha en dd/mm/aaaa: ")
+
+inicial = datetime.strptime(fecha_1, "%d/%m/%Y")
+final =   datetime.strptime(fecha_2, "%d/%m/%Y")
+
+# Obtener el intervalo entre dos fechas
+diferencia = relativedelta.relativedelta(final, inicial)
+
+print('El intervalo entre dos años es: ', diferencia.years)
+
+print('La diferencia completa entre los dos datos es: ')
+print(diferencia.years , ' años, ', diferencia.months, ' meses y ', diferencia.days, ' días')
+
