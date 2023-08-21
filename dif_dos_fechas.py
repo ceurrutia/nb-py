3275accd5aa19f81802a935208ab2f00ca5532cd
Cecilia urrutia <urrutia.cici@gmail.com>
2023-08-20 22:05:21 -0300

    Documento relative delta

--- dif_dos_fechas.py
+++ dif_dos_fechas.py
@@ -9,11 +9,10 @@ fecha_2 = input("Ingrese la segunda fecha en dd/mm/aaaa: ")
 inicial = datetime.strptime(fecha_1, "%d/%m/%Y")
 final =   datetime.strptime(fecha_2, "%d/%m/%Y")
 
-# Obtener el intervalo entre dos fechas
+# Obtener el intervalo entre dos fechas, libreria relative delta, permite intervalos restando la final de la inicial
 diferencia = relativedelta.relativedelta(final, inicial)
 
 print('El intervalo entre dos años es: ', diferencia.years)
 
 print('La diferencia completa entre los dos datos es: ')
 print(diferencia.years , ' años, ', diferencia.months, ' meses y ', diferencia.days, ' días')
-
